405fdbf949615e250d348602ee9e0e993d52914b
Irina Molodtsova <irinamolodtsova98@gmail.com>
2025-02-27 13:17:45 +0300

    lesson 12

--- src/test/java/lesson12_jenkinsParams/helpers/JenkinsProperties.java
+++ src/test/java/lesson12_jenkinsParams/helpers/JenkinsProperties.java
@@ -2,14 +2,13 @@ package lesson12_jenkinsParams.helpers;
 
 public class JenkinsProperties {
 
-    public static String getServer(){
+    public static String getServer() {
         String login = System.getProperty("login");
         String pw = System.getProperty("pw");
         String server = System.getProperty("server");
 
-        return "https://" + login + ":" + pw + "@" + server; //https://user1:1234@selenoid.autotests.cloud/wd/hub
+        return "https://" + login + ":" + pw + "@" + server + "/wd/hub";//https://user1:1234@selenoid.autotests.cloud/wd/hub    }
     }
-
     public static String getBrowser(){
         return System.getProperty("browser","chrome");
     }
