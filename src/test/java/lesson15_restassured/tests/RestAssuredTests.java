5d2ad8be7eca2dfa738a0ca3b2d71038fb2e0e12
Irina Molodtsova <irinamolodtsova98@gmail.com>
2025-03-17 18:35:02 +0300

    lesson 15

---
+++ src/test/java/lesson15_restassured/tests/RestAssuredTests.java
@@ -0,0 +1,95 @@
+package lesson15_restassured.tests;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class RestAssuredTests {
+
+    @Test
+    void userNotFoundTest() {
+        given()
+                .log().uri()
+                .get("https://reqres.in/api/users/23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+
+    @Test
+    void successLoginTest() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\"}";
+        given()
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void unsuccessfulLoginTest() {
+        String authData = "{\"email\": \"eve.holt@reqres.in\"}";
+        given()
+                .body(authData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .post("https://reqres.in/api/login")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    void updateUserDataPutMethodTest(){
+        String newData = "{\"name\": \"morpheus\", \"job\": \"zion resident\"}";
+        given()
+                .body( newData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .put("https://reqres.in/api/users/2")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("job", is("zion resident"));
+    }
+
+    @Test
+    void updateUserDataPatchMethodTest(){
+        String newData = "{\"name\": \"morpheus\", \"job\": \"zion resident\"}";
+        given()
+                .body( newData)
+                .contentType(JSON)
+                .log().uri()
+
+                .when()
+                .patch("https://reqres.in/api/users/2")
+
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .body("job", is("zion resident"));
+    }
+}
+
+
